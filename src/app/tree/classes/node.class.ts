dd7614c4db7a81e4454e388b09a0b3f41cb9e801
miker <mike_rivera@ultimatesoftware.com>
2019-07-31 23:48:33 -0400

    Fix NG build --prod error, added properties for isMaker in both node.class and node-maker.class  and reversed the boolean value for node.class.ts

--- src/app/tree/classes/node.class.ts
+++ src/app/tree/classes/node.class.ts
@@ -1,6 +1,13 @@
 import {TreeDiagramNodesList} from './nodesList.class'
 
 export class TreeDiagramNode {
+
+    private _isMaker = false;
+
+    public get isMaker () {
+        return this._isMaker;
+    }
+
     public parentId: string | null;
     public guid: string;
     public width: number;
